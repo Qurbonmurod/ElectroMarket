99e7fc44f4e13a0b67dd80d79c0daad1784f0fef
Sadikov Ravshan <RavshanCoder@gmail.com>
2021-12-23 13:15:34 +0500

    Add login, registration, help, contact and about pages

--- eshop.DataAccess/Models/Enums/Categories.cs
+++ eshop.DataAccess/Models/Enums/Categories.cs
@@ -8,7 +8,9 @@ namespace eshop.DataAccess.Enums
     {
         Barchasi,
         Kompyuterlar,
+        Printerlar,
         Aksessuarlar,
-        Telefonlar
+        Kompyuter_qismlari,
+        Boshqalar
     }
 }
--- eshop/Controllers/HomeController.cs
+++ eshop/Controllers/HomeController.cs
@@ -44,6 +44,36 @@ namespace eshop.Controllers
             return View();
         }
 
+        public IActionResult Checkout()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
